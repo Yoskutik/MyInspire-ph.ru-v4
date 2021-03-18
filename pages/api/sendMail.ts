4989c2578ea94cb2eea7eb90d2213ad0c6d94d32
Yoskutik <yosktuik@gmail.com>
2021-03-18 11:17:00 +0300

    asd

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -4,11 +4,9 @@ import { NotificationBot } from '@utils/NotificationBot';
 export default function sendMail(req: NextApiRequest, res: NextApiResponse): void {
     const data = JSON.parse(req.body);
     let message = `Новое сообщение от "${data.name.trim()}":\r\n`;
-    data.tel && (message += `тел.: ${data.tel}\r\n`);
-    data.email && (message += `email: ${data.email}\r\n`);
-    message += `--------------------
-${data.message}
-`;
+    data.tel && (message += `тел.: ${data.tel.trim()}\r\n`);
+    data.email && (message += `email: ${data.email.trim()}\r\n`);
+    message += `\r\n\r\n${data.message.trim()}`;
     new NotificationBot().sendToAll(message)
         .then(() => res.status(200).json({ success: true }))
         .catch(e => res.status(410).json({ success: false, message: e.code }));
